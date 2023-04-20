aaf6c593c7f483ecc0664bf5c208aee9a9b0bfb0
liyoi <2634659226@qq.com>
2023-04-20 18:32:25 +0800

    fixed:优化结果显示

--- pre.py
+++ pre.py
@@ -4,6 +4,11 @@ import torch
 import model
 
 
+AZ_labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+          'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+          'W', 'X', 'Y', 'Z']
+
 # 预测模块
 
 # 预测 0-9数字
@@ -22,7 +27,6 @@ def test_model(urls=None):
         return
     imgs_tensor = model.pre_deal_img(imgs, (28, 28))
     print(imgs_tensor.size())
-    print(imgs_tensor)
     imgs_tensor = imgs_tensor.float()
     test_net.to(model.device)
     predict = model.predict(test_net, imgs_tensor, model.device)
@@ -45,12 +49,19 @@ def test_AZ_model(urls=None):
         return
     imgs_tensor = model.pre_deal_img(imgs, (28, 28))
     print(imgs_tensor.size())
-    print(imgs_tensor)
     imgs_tensor = imgs_tensor.float()
     test_net.to(model.device)
     predict = model.predict(test_net, imgs_tensor, model.device)
-    print("预测结果", predict)
+    res=[]
+    if type(urls) is list:
+        size=urls.__len__()
+        for i in range(size):
+            print(predict[i].item())
+            res.append(AZ_labels[int(predict[i].item())])
+    else:
+        res.append(predict.item())
+    print("预测结果",res)
 
 
-test_model(["mnist_data/MNIST/raw/test/8.jpg"])
-test_AZ_model(["data/testset_28_28/W/W_1.jpg"])
+# test_model(["mnist_data/MNIST/raw/test/8.jpg"])
+test_AZ_model(["data/testset_28_28/W/W_1.jpg","data/testset_28_28/9/9_1.jpg","data/testset_28_28/D/D_1.jpg"])
